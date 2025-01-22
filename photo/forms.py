82e85a793c6acb1b31fb63f75b7975308d439cc5
y-ooe <fko2447054@stu.o-hara.ac.jp>
2025-01-22 14:45:12 +0900

    sakujo

--- photo/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from .models import PhotoPost
-
-class PhotoPostForm(ModelForm):
-    class Meta:
-
-        model = PhotoPost
-
-        fields = ['category', 'title', 'comment', 'image1', 'image2']
